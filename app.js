719e6c63749fc7bbd54af8d3cd97d2d89a813123
wtsninnt <32898860+wtsninnt@users.noreply.github.com>
2020-02-02 01:35:25 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+$(function() {
+
+    // Fixed Header =================
+
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH
+    let scrollPos = $(window).scrollTop()
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+
+    $(window).on("scroll load resize", function() {
+        let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    });
+
+    // Smooth Scroll ===============
+
+    $("[data-scroll]").on("click", function(event) {
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 800);
+
+    });
+
+    // Nav Toggle ==================
+
+    navToggle.on("click", function(event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+
+    // Reviews =====================
+    // https://kenwheeler.github.io/slick/
+
+    let slider = $("#reviewsSlider")
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false
+    });
+
+});
